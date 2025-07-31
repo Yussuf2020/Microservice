09d4463a452f73732be95d2aea394bfae382d5a0
Yussuf2020 <ajibolayussuf2020@gmai.com>
2025-07-31 11:49:33 +0100

    Changing Price

--- bisi/index.js
+++ bisi/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 // Get all products
 app.get('/api/products',(req,res)=>{
     const products = [
-        {id:1,name:'Laptop',price:2000},
+        {id:1,name:'Spagetti',price:50000},
         {id:2,name:'Desktop',price:2500},
         {id:3,name:'Phone',price:900},
         {id:4,name:'Tv',price:3000},
